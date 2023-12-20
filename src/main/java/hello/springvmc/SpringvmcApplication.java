1cc901a570abe04bddd4aa9042b9fe191691771a
MINmin-e <weluv.ww@gmail.com>
2023-12-20 10:17:34 +0900

    Initial commit

---
+++ src/main/java/hello/springvmc/SpringvmcApplication.java
@@ -0,0 +1,13 @@
+package hello.springvmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringvmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringvmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springvmc/SpringvmcApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springvmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringvmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
