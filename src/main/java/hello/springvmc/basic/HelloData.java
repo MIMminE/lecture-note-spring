81b14b0b3e8c2fdc5621ccfb29a4a872be660d4f
너츠 <weluv.ww@gmail.com>
2023-12-23 10:29:45 +0900

    2023. 12. 23

---
+++ src/main/java/hello/springvmc/basic/HelloData.java
@@ -0,0 +1,9 @@
+package hello.springvmc.basic;
+
+import lombok.Data;
+
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
---
+++ src/main/java/hello/springvmc/basic/request/RequestModelAttributeController.java
@@ -0,0 +1,27 @@
+package hello.springvmc.basic.request;
+
+import hello.springvmc.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class RequestModelAttributeController {
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+}
