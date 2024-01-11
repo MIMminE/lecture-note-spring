2fbf4fc44b10aa4215c660a63e87ab66f7231a7d
MINmin-e <weluv.ww@gmail.com>
2024-01-11 12:01:27 +0900

    API 예외 처리, 전역 Advice 구현

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,14 +1,12 @@
 package hello.exception.api;
 
 import hello.exception.exception.BadReqestException;
+import hello.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
@@ -17,25 +15,30 @@ import org.springframework.web.server.ResponseStatusException;
 public class ApiExceptionController {
 
     @GetMapping("/members/{id}")
-    public MemberDto getMember(@PathVariable("id") String id){
-        if (id.equals("ex")){
+    public MemberDto getMember(@PathVariable("id") String id) {
+        if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
         }
-        if (id.equals("bad")){
+        if (id.equals("bad")) {
             throw new BadReqestException();
         }
         return new MemberDto(id, "hello " + id);
     }
 
     @GetMapping("/response-status-ex2")
-    public String responseStatus(){
+    public String responseStatus() {
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
+    @GetMapping("/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
     }
 
     // DTO는 데이터 전송 계층의 역할로 데이터를 간소화하여 전송하기 위한 목적에서 사용되는 오브젝트
     @Data
     @AllArgsConstructor
-    static class MemberDto{
+    static class MemberDto {
         private String memberId;
         private String name;
     }
