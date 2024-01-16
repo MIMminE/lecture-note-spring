25b74ef293d1ac1f088a961898b35eee0d8c1fb3
너츠 <weluv.ww@gmail.com>
2024-01-16 20:58:52 +0900

    HttpServletRequest 파일 업로드 컨트롤러 작성

--- src/main/java/spring_mvc2/upload/Controller/ServletUploadControllerV1.java
+++ src/main/java/spring_mvc2/upload/Controller/ServletUploadControllerV1.java
@@ -1,2 +1,32 @@
-package spring_mvc2.upload.Controller;public class ServletUploadControllerV1 {
+package spring_mvc2.upload.Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Part;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v1")
+public class ServletUploadControllerV1 {
+
+    @GetMapping("/upload")
+    public String newFile(){
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFileV2(HttpServletRequest request) throws ServletException, IOException {
+        String itemName = request.getParameter("itemName");
+        Collection<Part> parts = request.getParts();
+
+        return "upload-form";
+    }
 }
