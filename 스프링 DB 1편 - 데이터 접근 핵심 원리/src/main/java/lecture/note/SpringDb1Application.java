2f1871c49886af4346bd34956b8872dc8ebb6213
너츠 <weluv.ww@gmail.com>
2024-03-11 20:07:03 +0900

    Update 스프링 DB 1편 - 데이터 접근 핵심 원리, README

---
+++ 스프링 DB 1편 - 데이터 접근 핵심 원리/src/main/java/lecture/note/SpringDb1Application.java
@@ -0,0 +1,13 @@
+package lecture.note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDb1Application.class, args);
+    }
+
+}
---
+++ 스프링 DB 1편 - 데이터 접근 핵심 원리/src/test/java/lecture/note/SpringDb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package lecture.note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
