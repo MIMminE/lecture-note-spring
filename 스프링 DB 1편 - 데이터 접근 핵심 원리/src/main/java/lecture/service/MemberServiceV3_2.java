3c29fb0cd25a01a2b1fc51896a70904983355209
MIMminE <weluv.ww@gmail.com>
2024-03-15 08:12:30 +0900

    TransactionTemplate 이용한 트랜잭션 문제 해결 개선

---
+++ 스프링 DB 1편 - 데이터 접근 핵심 원리/src/main/java/lecture/service/MemberServiceV3_2.java
@@ -0,0 +1,48 @@
+package lecture.service;
+
+import lecture.domain.Member;
+import lecture.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+public class MemberServiceV3_2 {
+
+    private final TransactionTemplate txTemplate;
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        txTemplate.executeWithoutResult((status -> {
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (Exception e) {
+                throw new IllegalStateException(e);
+            }
+        }));
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, fromMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
