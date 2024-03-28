963b2b73e399ff5add87bb01948fe7a209e19d97
MIMminE <weluv.ww@gmail.com>
2024-03-28 12:42:15 +0900

    고급편 통합

---
+++ 스프링 핵심 원리 - 고급편/src/main/java/hello/advanced/app/GitTestClass.java
@@ -0,0 +1,4 @@
+package hello.advanced.app;
+
+public class GitTestClass {
+}
--- 스프링 핵심 원리 - 고급편/src/test/java/hello/advanced/AdvancedApplicationTests.java
+++ 스프링 핵심 원리 - 고급편/src/test/java/hello/advanced/AdvancedApplicationTests.java
@@ -1,4 +1,4 @@
-package study.spring.advanced;
+package hello.advanced;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
