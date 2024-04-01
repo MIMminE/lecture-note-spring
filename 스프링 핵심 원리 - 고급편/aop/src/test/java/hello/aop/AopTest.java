92aa5220971ac149907a5b27d1395da777fb7659
MIMminE <weluv.ww@gmail.com>
2024-04-01 15:13:42 +0900

    스프링 AOP 구현4 - 포인트컷 참조

--- 스프링 핵심 원리 - 고급편/aop/src/test/java/hello/aop/AopTest.java
+++ 스프링 핵심 원리 - 고급편/aop/src/test/java/hello/aop/AopTest.java
@@ -4,6 +4,7 @@ import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV2;
 import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4PointCut;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV3.class)
+@Import(AspectV4PointCut.class)
 public class AopTest {
 
     @Autowired
